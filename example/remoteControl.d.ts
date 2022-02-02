f7130c36fdb9445dd666b4c42b2e7c1ff9e313c5
SilentTiger <cia.fbi.007@hotmail.com>
2022-02-02 14:19:14 +0000

    Deploying to gh-pages from @ SilentTiger/laser-pen@0b3bd7649e1e7bc8d50e38bf6ef2271e407416e9 🚀

--- example/remoteControl.d.ts
+++ example/remoteControl.d.ts
@@ -1,12 +1,1 @@
-declare class Peer {
-    on(eventName: string, callback: (...args: any[]) => void): void;
-    connect(id: string, arg?: {
-        reliable: boolean;
-    }): PeerConnection;
-}
-declare class PeerConnection {
-    on(eventName: string, callback: (...args: any[]) => void): void;
-    send(msg: string): void;
-}
-declare function acceptRemoteControl(): void;
-declare function startRemoteControl(): void;
+export {};
